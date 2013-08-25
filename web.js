1e7280d15efefba498b49ef6a048a7dc36a757b8
Tim Shim <timshim@gmail.com>
2013-08-25 16:48:08 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
-var app = express();
-app.use(express.logger());
+
+var app = express.createServer(express.logger());
+
+var data = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send("index.html");
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
